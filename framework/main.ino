37f2b28fb3a0df878ebb7450e3a42971631b91d2
sk233 <9653231+shike233@user.noreply.gitee.com>
2024-10-12 19:21:00 +0800

    Update PS2.lnp, README.TXT, PS2.plg, README.TXT, TEST.plg, README.TXT, keilkilll.bat, keilkilll.bat, keilkilll.bat, TEST.sct, TEST.sct, key.h, led.h, led.h, led.h, led.c, led.c, led.c, servo.h

---
+++ framework/main.ino
@@ -0,0 +1,301 @@
+#define Montor_FR_IN1 8//Motor1 Direction FR为右前
+#define Montor_FR_IN2 7//Motor1 Direction
+#define Montor_FL_IN3 2//Motor2 Direction FL为左前
+#define Montor_FL_IN4 4//Motor2 Direction
+#define Montor_BR_IN5 14//Motor3 Direction BR为右后
+#define Montor_BR_IN6 15//Motor3 Direction
+#define Montor_BL_IN7 17//Motor4 Direction BL为左后
+#define Montor_BL_IN8 16//Motor4 Direction
+#define Montor_FR_ENA 9//M SPREED
+#define Montor_FL_ENB 6//M SPREED
+#define Montor_BR_ENC 5//M SPREED
+#define Montor_BL_END 3//M SPREED
+//占空比取值：0~VALL
+const int VA;
+const int VB;
+const int VC;
+const int VD;
+const int VALL=255;
+//统一速度
+void SPEED_SETTING(char A,char B,char C,char D)
+{
+  analogWrite(Montor_FR_ENA,A);
+  analogWrite(Montor_FL_ENB,B);
+  analogWrite(Montor_BR_ENC,C);
+  analogWrite(Montor_BL_END,D);
+}
+//小车向前运动
+void FRONTWARD(char VALL)
+{
+ //右前轮向前运动
+ digitalWrite(Montor_FR_IN1,LOW);
+ digitalWrite(Montor_FR_IN2,HIGH);
+ analogWrite(Montor_FR_ENA,VALL);
+ //左前轮向前运动
+ digitalWrite(Montor_FL_IN3,LOW);
+ digitalWrite(Montor_FL_IN4,HIGH);
+ analogWrite(Montor_FL_ENB,VALL);
+ //右后轮向前运动
+ digitalWrite(Montor_BR_IN5,LOW);
+ digitalWrite(Montor_BR_IN6,HIGH);
+ analogWrite(Montor_BR_ENC,VALL);
+ //左后轮向前运动
+ digitalWrite(Montor_BL_IN7,LOW);
+ digitalWrite(Montor_BL_IN8,HIGH);
+ analogWrite(Montor_BL_END,VALL);
+} 
+//小车向后运动
+void BACKWARD(char VALL)
+{
+ //右前轮向后运动
+ digitalWrite(Montor_FR_IN1,HIGH);
+ digitalWrite(Montor_FR_IN2,LOW);
+ analogWrite(Montor_FR_ENA,VALL);
+ //左前轮向后运动
+ digitalWrite(Montor_FL_IN3,HIGH);
+ digitalWrite(Montor_FL_IN4,LOW);
+ analogWrite(Montor_FL_ENB,VALL);
+ //右后轮向后运动
+ digitalWrite(Montor_BR_IN5,HIGH);
+ digitalWrite(Montor_BR_IN6,LOW);
+ analogWrite(Montor_BR_ENC,VALL);
+ //左后轮向后运动
+ digitalWrite(Montor_BL_IN7,HIGH);
+ digitalWrite(Montor_BL_IN8,LOW);
+ analogWrite(Montor_BL_END,VALL);
+} 
+//小车向左运动
+void LEFTWARD(char VALL)
+{
+ //左前轮向后运动
+ digitalWrite(Montor_FL_IN3,HIGH);
+ digitalWrite(Montor_FL_IN4,LOW);
+ analogWrite(Montor_FL_ENB,VALL);
+ //左后轮向前运动
+ digitalWrite(Montor_BL_IN7,LOW);
+ digitalWrite(Montor_BL_IN8,HIGH);
+ analogWrite(Montor_BL_END,VALL);
+ //右前轮向前运动
+ digitalWrite(Montor_FR_IN1,LOW);
+ digitalWrite(Montor_FR_IN2,HIGH);
+ analogWrite(Montor_FR_ENA,VALL);
+ //右后轮向后运动
+ digitalWrite(Montor_BR_IN5,HIGH);
+ digitalWrite(Montor_BR_IN6,LOW);
+ analogWrite(Montor_BR_ENC,VALL);
+}
+//小车向右运动
+void RIGHTWARD(char VALL)
+{
+  //右前轮向后运动
+ digitalWrite(Montor_FR_IN1,HIGH);
+ digitalWrite(Montor_FR_IN2,LOW);
+ analogWrite(Montor_FR_ENA,VALL);
+  //右后轮向前运动
+ digitalWrite(Montor_BR_IN5,LOW);
+ digitalWrite(Montor_BR_IN6,HIGH);
+ analogWrite(Montor_BR_ENC,VALL);
+  //左前轮向前运动
+ digitalWrite(Montor_FL_IN3,LOW);
+ digitalWrite(Montor_FL_IN4,HIGH);
+ analogWrite(Montor_FL_ENB,VALL);
+  //左后轮向后运动
+ digitalWrite(Montor_BL_IN7,HIGH);
+ digitalWrite(Montor_BL_IN8,LOW);
+ analogWrite(Montor_BL_END,VALL);
+}
+//小车向左前运动
+void LEFT_FRONTWARD(char VALL)
+{
+  //右前轮向前运动
+ digitalWrite(Montor_FR_IN1,LOW);
+ digitalWrite(Montor_FR_IN2,HIGH);
+ analogWrite(Montor_FR_ENA,VALL);
+  //左后轮向前运动
+ digitalWrite(Montor_BL_IN7,LOW);
+ digitalWrite(Montor_BL_IN8,HIGH);
+ analogWrite(Montor_BL_END,VALL);
+ //右后轮静止
+ digitalWrite(Montor_BR_IN5,LOW);
+ digitalWrite(Montor_BR_IN6,LOW);
+ analogWrite(Montor_BR_ENC,VALL);
+ //左前轮静止
+ digitalWrite(Montor_FL_IN3,LOW);
+ digitalWrite(Montor_FL_IN4,LOW);
+ analogWrite(Montor_FL_ENB,VALL);
+}
+//小车向左后运动
+void LEFT_BACKWARD(char VALL)
+{
+  //右前轮向后运动
+ digitalWrite(Montor_FR_IN1,HIGH);
+ digitalWrite(Montor_FR_IN2,LOW);
+ analogWrite(Montor_FR_ENA,VALL);
+  //左后轮向后运动
+ digitalWrite(Montor_BL_IN7,HIGH);
+ digitalWrite(Montor_BL_IN8,LOW);
+ analogWrite(Montor_BL_END,VALL);
+ //右后轮静止
+ digitalWrite(Montor_BR_IN5,LOW);
+ digitalWrite(Montor_BR_IN6,LOW);
+ analogWrite(Montor_BR_ENC,VALL);
+ //左前轮静止
+ digitalWrite(Montor_FL_IN3,LOW);
+ digitalWrite(Montor_FL_IN4,LOW);
+ analogWrite(Montor_FL_ENB,VALL);
+}
+//小车向右前运动
+void RIGHT_FRONTWARD(char VALL)
+{
+  //左前轮向前运动
+ digitalWrite(Montor_FL_IN3,LOW);
+ digitalWrite(Montor_FL_IN4,HIGH);
+ analogWrite(Montor_FL_ENB,VALL);
+  //右后轮向前运动
+ digitalWrite(Montor_BR_IN5,LOW);
+ digitalWrite(Montor_BR_IN6,HIGH);
+ analogWrite(Montor_BR_ENC,VALL);
+ //左后轮静止
+ digitalWrite(Montor_BL_IN7,LOW);
+ digitalWrite(Montor_BL_IN8,LOW);
+ analogWrite(Montor_BL_END,VALL);
+ //右前轮静止
+ digitalWrite(Montor_FR_IN1,LOW);
+ digitalWrite(Montor_FR_IN2,LOW);
+ analogWrite(Montor_FR_ENA,VALL);
+}
+//小车向右后运动
+void RIGHT_BACKWARD(char VALL)
+{
+  //左前轮向后运动
+ digitalWrite(Montor_FL_IN3,HIGH);
+ digitalWrite(Montor_FL_IN4,LOW);
+ analogWrite(Montor_FL_ENB,VALL);
+  //右后轮向后运动
+ digitalWrite(Montor_BR_IN5,HIGH);
+ digitalWrite(Montor_BR_IN6,LOW);
+ analogWrite(Montor_BR_ENC,VALL);
+ //右前轮静止
+ digitalWrite(Montor_FR_IN1,LOW);
+ digitalWrite(Montor_FR_IN2,LOW);
+ analogWrite(Montor_FR_ENA,VALL);
+ //左后轮精止
+ digitalWrite(Montor_BL_IN7,LOW);
+ digitalWrite(Montor_BL_IN8,LOW);
+ analogWrite(Montor_BL_END,VALL);
+}
+//小车停止
+void STOP()
+{
+ digitalWrite(Montor_FR_IN1,LOW);
+ digitalWrite(Montor_FR_IN2,LOW);
+ digitalWrite(Montor_BL_IN7,LOW);
+ digitalWrite(Montor_BL_IN8,LOW);
+ digitalWrite(Montor_BR_IN5,LOW);
+ digitalWrite(Montor_BR_IN6,LOW);
+ digitalWrite(Montor_FL_IN3,LOW);
+ digitalWrite(Montor_FL_IN4,LOW);
+ analogWrite(Montor_FR_ENA,0);
+ analogWrite(Montor_FL_ENB,0);
+ analogWrite(Montor_BR_ENC,0);
+ analogWrite(Montor_BL_END,0);
+}
+//小车原地向右转
+void RIGHT_TURNAROUND(char VALL)
+{
+  //左前轮向前运动
+ digitalWrite(Montor_FL_IN3,LOW);
+ digitalWrite(Montor_FL_IN4,HIGH);
+ analogWrite(Montor_FL_ENB,VALL);
+  //右前轮向后运动
+ digitalWrite(Montor_FR_IN1,HIGH);
+ digitalWrite(Montor_FR_IN2,LOW);
+ analogWrite(Montor_FR_ENA,VALL);
+  //左后轮向前运动
+ digitalWrite(Montor_BR_IN5,LOW);
+ digitalWrite(Montor_BR_IN6,HIGH);
+ analogWrite(Montor_BR_ENC,VALL);
+  //右后轮向后运动
+ digitalWrite(Montor_BR_IN5,HIGH);
+ digitalWrite(Montor_BR_IN6,LOW);
+ analogWrite(Montor_BR_ENC,VALL);
+}
+//小车原地左转
+void LEFT_TURNAROUND(char VALL)
+{
+  //左前轮向后运动
+ digitalWrite(Montor_FL_IN3,HIGH);
+ digitalWrite(Montor_FL_IN4,LOW);
+ analogWrite(Montor_FL_ENB,VALL);
+  //右前轮向前运动
+ digitalWrite(Montor_FR_IN1,LOW);
+ digitalWrite(Montor_FR_IN2,HIGH);
+ analogWrite(Montor_FR_ENA,VALL);
+  //左后轮向后运动
+ digitalWrite(Montor_BR_IN5,HIGH);
+ digitalWrite(Montor_BR_IN6,LOW);
+ analogWrite(Montor_BR_ENC,VALL);
+  //右后轮向前运动
+ digitalWrite(Montor_BR_IN5,LOW);
+ digitalWrite(Montor_BR_IN6,HIGH);
+ analogWrite(Montor_BR_ENC,VALL);
+}
+//将引脚设置为输出模式
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(5,OUTPUT);
+}
+void loop()
+{
+  BACKWARD(VALL);
+  delay(2000);
+  STOP();
+  delay(2000);
+  FRONTWARD(VALL);
+  delay(2000);
+  STOP();
+  delay(2000);
+  LEFTWARD(VALL);
+  delay(2000);
+  STOP();
+  delay(2000);
+  RIGHTWARD(VALL);
+  delay(2000);
+  STOP();
+  delay(2000);
+  LEFT_FRONTWARD(VALL);
+  delay(2000);
+  STOP();
+  delay(2000);
+  LEFT_BACKWARD(VALL);
+  delay(2000);
+  STOP();
+  delay(2000);
+  RIGHT_FRONTWARD(VALL);
+  delay(2000);
+  STOP();
+  delay(2000);
+  RIGHT_BACKWARD(VALL);
+  delay(2000);
+  STOP();
+  delay(2000);
+  RIGHT_TURNAROUND(VALL);
+  delay(2000);
+  STOP();
+  delay(2000);
+  LEFT_TURNAROUND(VALL);
+  delay(2000);
+  STOP();
+  delay(2000);
+}
